1132f8418d66c9e3ecfaac5182899b87736e51bf
Taisei <taisei96.web@gmail.com>
2023-12-05 07:55:58 +0900

    first commit

---
+++ section2/index.ts
@@ -0,0 +1,26 @@
+let hasValue: boolean = false;
+
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12
+
+let single: string = 'hello';
+let double: string = "hello"
+let back: string = `hello`;
+
+// オブジェクト
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Jack',
+  age: 21
+}
+
+// 配列
+const fruits: string[] = ['Apple', 'Banana', 'Grape']
+
+// Tuple
+const book: [string, number, boolean] = ['business', 1500, false]
+
+// Enum
