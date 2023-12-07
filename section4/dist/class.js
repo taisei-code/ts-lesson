c0b6061592a3157ceb920256f4727179018fe996
Taisei <taisei96.web@gmail.com>
2023-12-07 09:05:22 +0900

    修飾語

--- section4/dist/class.js
+++ section4/dist/class.js
@@ -1,18 +1,18 @@
 "use strict";
 class Person {
-    constructor(initName) {
+    constructor(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
     }
     // メソット
+    incrementAge() {
+        this.age += 1;
+    }
     greeting() {
-        console.log(`Hello My name is ${this.name}`);
+        console.log(`Hello My name is ${this.name}. I am ${this.age} years old`);
     }
 }
 // インスタンス（オブジェクト）生成
-const quill = new Person('Quill');
+const quill = new Person('Quill', 38);
+quill.incrementAge();
 quill.greeting();
-const anotherQuill = {
-    name: "anotherTaisei",
-    greeting: quill.greeting
-};
-anotherQuill.greeting();
--- section4/src/class.ts
+++ section4/src/class.ts
@@ -1,24 +1,25 @@
 class Person {
   name: string;
+  private age: number;
 
-  constructor(initName: string) {
+  constructor(initName: string, initAge: number) {
     this.name = initName
+    this.age  = initAge
   }
 
   // メソット
+  incrementAge() {
+    this.age += 1;
+  }
+
   greeting(this: Person) {
-    console.log(`Hello My name is ${this.name}`)
+    console.log(`Hello My name is ${this.name}. I am ${this.age} years old`)
   }
 
 }
 
 // インスタンス（オブジェクト）生成
-const quill = new Person('Quill');
+const quill = new Person('Quill', 38);
+quill.incrementAge()
 quill.greeting()
 
-const anotherQuill = {
-  name: "anotherTaisei",
-  greeting: quill.greeting
-}
-
-anotherQuill.greeting()
